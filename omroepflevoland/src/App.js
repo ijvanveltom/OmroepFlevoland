4c6cd31bf3c8b2c7038ab48e17fb5ecfae0c355b
Julie <wariostarx@gmail.com>
2022-04-25 15:30:16 +0200

    Added main grid and aligned them like the Xd

--- omroepflevoland/src/App.js
+++ omroepflevoland/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import TextArea from './components/TextArea';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Toolbar position="absolute" sx={{height: '7vh', overflow: 'hidden',}}>
+          <h1>Dashboard </h1>
+        </Toolbar>
+        <Box sx={{width:'100%', height: '93vh', overflow: 'hidden', display: 'flex',}}>
+          <Box sx={{width:'4%', height: '100%', overflow: 'auto', backgroundColor: 'primary.light'}}>
+          </Box>
+          <Box sx={{width:'20%', height: '100%', overflow: 'auto',}}>
+            <Button variant="contained">Hello World</Button>
+          </Box>
+          <Box sx={{width:'76%', height: '100%', overflow: 'hidden',}}>
+            <Box sx={{width:'100%', height: '30%', overflow: 'auto', backgroundColor: 'secondary.light'}}>
+            </Box>
+            <Box sx={{display: 'flex', flexDirection: 'row', width:'100%', height: '70%', overflow: 'hidden',}}>
+              <Box sx={{display: 'flex', width:'50%', height: '100%', overflow: 'auto', backgroundColor: 'primary.dark'}}>
+                  <TextArea></TextArea>
+              </Box>
+              <Box sx={{display: 'flex', width:'50%', height: '100%', overflow: 'auto', backgroundColor: 'secondary.dark'}}>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
     </div>
   );
 }
