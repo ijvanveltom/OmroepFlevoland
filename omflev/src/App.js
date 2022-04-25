50927da4e30c6b99ed31ee501deb9bc0d8c86a7c
ijvanveltom <403283@student.fontys.nl>
2022-04-25 11:18:33 +0200

    :tada: Start new React app
    Due the complications of the previous version, I decided to rebuild the app and start clean again.

---
+++ omflev/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <Button variant="contained">Hello World</Button>
+  );
+}
+
+export default App;
