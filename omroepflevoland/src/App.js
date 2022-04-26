9a8ccc8a0d8cc03531429b5fb507516f8d50458b
Julie <wariostarx@gmail.com>
2022-04-26 13:35:34 +0200

    Added 2 rich text editors

--- omroepflevoland/src/App.js
+++ omroepflevoland/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import TextArea from './components/TextArea';
+
+import React, { useState } from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function App() {
+  const [valueLeft, setValueLeft] = useState('');
+  const [valueRight, setValueRight] = useState('');
+
   return (
     <div className="App">
         <Toolbar position="absolute" sx={{height: '7vh', overflow: 'hidden',}}>
@@ -20,10 +27,11 @@ function App() {
             <Box sx={{width:'100%', height: '30%', overflow: 'auto', backgroundColor: 'secondary.light'}}>
             </Box>
             <Box sx={{display: 'flex', flexDirection: 'row', width:'100%', height: '70%', overflow: 'hidden',}}>
-              <Box sx={{display: 'flex', width:'50%', height: '100%', overflow: 'auto', backgroundColor: 'primary.dark'}}>
-                  <TextArea></TextArea>
+              <Box sx={{display: 'flex', width:'50%', height: '100%', overflow: 'auto',}}>
+                <ReactQuill theme="snow" value={valueLeft} onChange={setValueLeft} sx={{width: '100%', height: '100%',}}/>
               </Box>
-              <Box sx={{display: 'flex', width:'50%', height: '100%', overflow: 'auto', backgroundColor: 'secondary.dark'}}>
+              <Box sx={{display: 'flex', width:'50%', height: '100%', overflow: 'auto',}}>
+                <ReactQuill theme="snow" value={valueRight} onChange={setValueRight} sx={{width: '100%', height: '100%',}}/>
               </Box>
             </Box>
           </Box>
