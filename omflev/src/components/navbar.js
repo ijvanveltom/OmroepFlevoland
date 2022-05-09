1e9106f9337f31139328204a9973ce40e8dc7d43
ijvanveltom <403283@student.fontys.nl>
2022-05-09 14:09:58 +0200

    :construction: Added timer

--- omflev/src/components/navbar.js
+++ omflev/src/components/navbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "../styles/navbar.css"
+import logo from "../styles/logo.png"
+import Clock from 'react-live-clock';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 const navbar = () => {
     return (
         <div className='navbar'>
+            <img className='logo' alt="logo" src={logo} />
             <li>
                 <Link to="/stories">Stories</Link>
             </li>
@@ -14,6 +18,15 @@ const navbar = () => {
             <li>
                 <Link to="/media">Media</Link>
             </li>
+            <div className='timer' style={{
+                display: 'flex',
+                alignItems: 'center',
+                flexWrap: 'wrap'
+            }}>
+                <AccessTimeIcon />
+                <Clock format={'HH:mm:ss'} ticking={true} timezone={'UTC/GMT +2 hours'} />
+            </div>
+            
         </div>
     );
 }
