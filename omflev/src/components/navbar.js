2b99ab32e68bad2880f3b83580ee87930b0c5bad
ijvanveltom <403283@student.fontys.nl>
2022-05-09 11:51:17 +0200

    :lipstick: UI / Design navbar

--- omflev/src/components/navbar.js
+++ omflev/src/components/navbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {  Link } from "react-router-dom";
-const navbar= () =>{
-  return (
-  <div>
-    <li>
-      <Link to="/stories">Stories</Link>
-    </li>
-    <li>
-      <Link to="/draaiboeken">Draaiboeken</Link>
-    </li>
-    <li>
-      <Link to="/media">Media</Link>
-    </li>
-  </div>
-  );
+import { Link } from "react-router-dom";
+import "../styles/navbar.css"
+
+const navbar = () => {
+    return (
+        <div className='navbar'>
+            <li>
+                <Link to="/stories">Stories</Link>
+            </li>
+            <li>
+                <Link to="/draaiboeken">Draaiboeken</Link>
+            </li>
+            <li>
+                <Link to="/media">Media</Link>
+            </li>
+        </div>
+    );
 }
 export default navbar;
