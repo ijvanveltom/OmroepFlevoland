f5f45ab1adb28305dbbf30b10fdf1d1bacfb6746
ijvanveltom <403283@student.fontys.nl>
2022-05-09 14:24:03 +0200

    :construction: Complete navbar functions
    Needs some styling

--- omflev/src/components/navbar.js
+++ omflev/src/components/navbar.js
@@ -3,30 +3,58 @@ import { Link } from "react-router-dom";
 import "../styles/navbar.css"
 import logo from "../styles/logo.png"
 import Clock from 'react-live-clock';
+
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import GridViewIcon from '@mui/icons-material/GridView';
+import CreateIcon from '@mui/icons-material/Create';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const navbar = () => {
     return (
         <div className='navbar'>
             <img className='logo' alt="logo" src={logo} />
-            <li>
-                <Link to="/stories">Stories</Link>
-            </li>
-            <li>
-                <Link to="/draaiboeken">Draaiboeken</Link>
-            </li>
-            <li>
-                <Link to="/media">Media</Link>
-            </li>
+            <div className='menu' style={{
+                display: 'flex',
+                alignItems: 'center',
+                flexWrap: 'wrap'
+            }}>
+                <li>
+                    <Link to="/stories">Stories</Link>
+                </li>
+                <li>
+                    <Link to="/draaiboeken">Draaiboeken</Link>
+                </li>
+                <li>
+                    <Link to="/media">Media</Link>
+                </li>
+            </div>
             <div className='timer' style={{
                 display: 'flex',
                 alignItems: 'center',
                 flexWrap: 'wrap'
             }}>
                 <AccessTimeIcon />
-                <Clock format={'HH:mm:ss'} ticking={true} timezone={'UTC/GMT +2 hours'} />
+                <Clock format={'HH:mm:ss'} ticking={true} timezone={'UTC/GMT'} />
             </div>
-            
+            <div className='settings' style={{
+                display: 'flex',
+                alignItems: 'center',
+                flexWrap: 'wrap'
+            }}>
+                <li>
+                    <Link to="/#"><QuestionMarkIcon /></Link>
+                </li>
+                <li>
+                    <Link to="/#"><GridViewIcon /></Link>
+                </li>
+                <li>
+                    <Link to="/#"><CreateIcon /></Link>
+                </li>
+                <li>
+                    <Link to="/#"><AccountCircleIcon /></Link>
+                </li>
+            </div>
         </div>
     );
 }
