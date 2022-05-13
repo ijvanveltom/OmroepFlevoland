de84d32a28032ec357dd1e6437175c7a212cad26
ijvanveltom <403283@student.fontys.nl>
2022-05-13 12:02:31 +0200

    :sparkles: Homepage

--- omflev/src/pages/home.js
+++ omflev/src/pages/home.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid } from '@mui/material';
+import "../styles/home.css";
 
-const Home = () =>{
-  return (
-    <div>
-      <h3>Home</h3>
-      <div>
-        <h4>Insert leuke home pagina</h4>
-      </div>
-    </div>
-  );
+const Home = () => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <h3 className='titel'>Home</h3>
+                <div className='bttn'>
+                    <Link to='/stories'>
+                        <button>Leuke Button naar stories</button>
+                    </Link>
+                    <Link to='/draaiboeken'>
+                        <button>Leuke Button naar draaiboeken</button>
+                    </Link>
+                    <Link to='/media'>
+                        <button>Leuke Button naar media</button>
+                    </Link>
+                </div>
+            </Grid>
+            <Grid item xs={6}>
+                <h3 className='titel'>Andere informatie</h3>
+            </Grid>
+        </Grid>
+    );
 }
 export default Home;
